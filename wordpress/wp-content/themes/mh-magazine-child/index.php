c14f2548cbe3274ff588ba7b347182ee83b81534
quentin <zhor.osu@gmail.com>
2017-05-03 11:39:53 +0200

    debut child theme

---
+++ wordpress/wp-content/themes/mh-magazine-child/index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<div class="mh-wrapper clearfix">
+    <div id="main-content" class="mh-loop mh-content" role="main">
+        <img src="http://lorempixel.com/600/300" />
+    </div>
+    <h2>Actualités</h2>
+    <section>
+        <article class="">
+            <img src="http://lorempixel.com/200/126" alt="">
+        </article>
+    </section>
+	<?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
